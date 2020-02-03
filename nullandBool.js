a889d87c517ba3bd92de8322a8528ef27dd9c8f0
John Hale <johnhale1976@gmail.com>
2020-02-03 10:31:46 -0500

    adding file

---
+++ nullandBool.js
@@ -0,0 +1,23 @@
+//temp id coming from website API
+
+var temp = 0
+
+//done some calculation
+//temp = 2
+
+console.log("current temperature is: " + temp);
+
+//Grade system
+//10 - Great jo//5 - work harder
+//2 - poor
+//0 - fail
+
+//Boolean - True/False
+
+// let grade = 7
+let myGrade = (actualMarkd == 10)
+
+
+console.log(myGrade);
+
+
