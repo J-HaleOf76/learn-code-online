77db0e52698ff908a820462d8e6703405dd13550
John Hale <johnhale1976@gmail.com>
2020-02-05 07:36:53 -0500

    if else practice

---
+++ basics2/ifElse.js
@@ -0,0 +1,60 @@
+// if (false) {
+//     console.log("it is false");
+    
+    
+           
+// } else {
+//     if (true) {
+//         console.log('Hello Its true');
+          
+// }
+// }
+
+
+// //LCO
+// let whoIsHere = "Diddo"
+
+// if (whoIsHere === 'user') {
+//     console.log('Greeting message for user')
+//     console.log('Allow access to view all courses')
+// } else if (whoIsHere === "teacher") {
+//     console.log('Greeting message for TEACHER')
+//     console.log('Allow access to his course')
+// }else {
+//     console.log('MESSAGE: please verify your email')
+//     console.log('Send user an email for verification')
+    
+// } 
+
+/*Grade problem
+This is the begining
+
+students marks:
+10 - Amazing
+5 - Good
+3 - Poor 
+0 - Fail*/ 
+
+let Grade = "2"
+
+if (Grade === "10") {
+    console.log('Great Job')
+    console.log('You did it!')
+    console.log('Persistance Is Key To Success')
+    
+    
+} else if (Grade === '5') {
+    console.log('50% Is No Good')
+    console.log('Practice Practice Practice')
+    
+} else if (Grade === '3') {
+    console.log('Very Poor')
+    
+} else if (Grade === '0') {
+    console.log('GO HOME Loser')
+    console.log('You Have Failed')
+    
+} else if (Grade = ' ') {
+    console.log("Enter A Grade InTo The Field")
+    
+}
