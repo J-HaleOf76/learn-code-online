8c45eed5c779e1467c4489850c501abc8261e788
John Hale <johnhale1976@gmail.com>
2020-02-07 12:50:56 -0500

    added files and folder

---
+++ basics2/scope.js
@@ -0,0 +1,33 @@
+// let iAmGlobal = 'someMoreValue'
+
+// if (true) {
+//     let iamLocal = 'someMoreValue'
+//     iAmGlobal = 'superMan'
+//     console.log(iAmGlobal);
+//     console.log(iamLocal);
+    
+    
+// }
+// console.log(iamLocal);
+// console.log(iAmGlobal);
+
+//Kings Problems
+
+let king = "KING: JohnJ"
+
+if (true) {
+    const king = 'Ram'
+
+    if (true) {
+        const king = 'Sam'
+        console.log(king);
+        
+    }
+}
+if (true) {
+    console.log("I am the kings right hand man... " + king);
+    
+    
+}
+
+
